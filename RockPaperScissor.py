7461ed593d596387dcb31849ca3dc83657375fb6
arpandiablo <unitedarpan@gmail.com>
2024-05-12 21:30:06 +0545

    Test

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,17 +1,20 @@
 import random
 options = ["Rock","Paper","Scissor"]
 results = [ ["Draw","You Lost","You Won"],["You Won","Draw","You Lost" ],["You Lost","You Won","Draw"]]
-userinp = input("Choose between Rock Paper and Scissor and \"Exit\" to Quit: ")
-compinp = random.choice(options)
 while True:
+    userinp = input("Choose between Rock Paper and Scissor and \"Exit\" to Quit: ")
+    compinp = random.choice(options)
     if userinp==compinp:
         print(f"Oops! Computer chose {compinp} too. The Game is Drawn")
     elif userinp=="Exit":
         print("Thanks for Playing")
         break
-    else:
-        flag = results[userinp][compinp]
+    elif userinp in options:
+        print(f"The Computer chose {compinp}.")
+        flag = results[options.index(userinp)][options.index(compinp)]
         print(f"{flag}")
+    else:
+        print("Invalid Option")
 
 #     R P S
 # R   D L W
